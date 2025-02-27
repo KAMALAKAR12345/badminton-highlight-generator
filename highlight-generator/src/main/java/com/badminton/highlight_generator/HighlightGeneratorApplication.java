e98f5360d8ad3ecf99c9655a238b4a266c09ed47
kamal <kamalakarthommandru9@gmail.com>
2025-02-27 14:24:41 +0530

    Initial commit with spring boot setup

---
+++ highlight-generator/src/main/java/com/badminton/highlight_generator/HighlightGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.badminton.highlight_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlightGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighlightGeneratorApplication.class, args);
+	}
+
+}
---
+++ highlight-generator/src/test/java/com/badminton/highlight_generator/HighlightGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badminton.highlight_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlightGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
