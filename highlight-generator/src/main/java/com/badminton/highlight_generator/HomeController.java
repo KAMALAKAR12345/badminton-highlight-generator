fd21ae9a7e44f5206b68eb6355351f8383cfbff7
kamal <kamalakarthommandru9@gmail.com>
2025-02-28 14:10:13 +0530

    Added Home controller to root URL mapping

---
+++ highlight-generator/src/main/java/com/badminton/highlight_generator/HomeController.java
@@ -0,0 +1,15 @@
+package com.badminton.highlight_generator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to the Automated Badminton Highlight Generator!";
+    }
+}
